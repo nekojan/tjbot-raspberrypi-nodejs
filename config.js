d34750ecc9a2789395d048a3bcc0c5d42893ff55
nekojan <nekojan1@hotmail.com>
2017-02-20 17:47:58 -0600

    add config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+// The attention word to wake up the robot.
+exports.attentionWord ='pi';
+
+// You can change the voice of the robot to your favorite voice.
+exports.voice = 'en-US_LisaVoice';
+// Some of the available options are:
+// en-US_AllisonVoice
+// en-US_LisaVoice
+// en-US_MichaelVoice (the default)
+
+// Credentials for Watson Speech to Text service
+exports.STTUsername = '2eaee30f-0a56-4ebf-80c2-cd105ce26c40';
+exports.STTPassword = 'b0EkYDuiCv3v';
+
+// Credentials for Watson Conversation service
+exports.ConUsername = '5e962b9c-daf0-4f45-92ff-58e9fc798e40';
+exports.ConPassword = 'viBweZUIZPqJ';
+exports.ConWorkspace = '96ca9b1f-0141-462e-a4e3-3971a7946c5c'
+
+//Credentials for Watson Tone Analyzer service
+exports.ToneUsername = 'f85b5ce6-72e0-45b8-bf33-a3d6bab34e01';
+exports.TonePassword = 'fqmkHZmr0gWo';
+
+//Credentials for Watson Text to Speech service
+exports.TTSUsername = '5fd19a0c-baa3-4200-8be6-ef4057180ad8';
+exports.TTSPassword = 'KhIbZoI3oTKV';
